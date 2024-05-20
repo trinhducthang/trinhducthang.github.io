edb03689ee627216e6f59e5682389ac5b83d4a9a
DucThang <139322881+trinhducthang@users.noreply.github.com>
2024-05-20 15:23:18 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript can be used to add interactivity or dynamic content if needed.
+// For this example, we don't need any additional JS.
+// However, here's an example of a simple function to log a message when the profile card is clicked.
+
+document.querySelector('.profile-card').addEventListener('click', function() {
+    console.log('Profile card clicked!');
+});
